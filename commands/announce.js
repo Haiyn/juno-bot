9739f452dab7e90f91254c75e366265326755b73
Haiyn <corexaren@gmail.com>
2018-12-12 21:21:39 +0100

    rewrote announce to send attachments

--- commands/announce.js
+++ commands/announce.js
@@ -1,24 +1,27 @@
 exports.run = (client, message, args) => {
-  if(args == "") {
-    client.logger.warn("Cannot run Command: announce (empty string)");
-    try {
-      msg.delete(0);
-    }
-    catch (ex) {
-      client.logger.warn("Could not delete message." + ex);
-    }
-  }
-  else {
+    const { Attachment } = require('discord.js');
     const text = args.slice().join(" ");
+    const urlregex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+
     try {
-      msg.delete(0);
+      message.delete(0);
     }
     catch (ex) {
-      client.logger.warn("Could not delete message." + ex);
+      client.logger.warn("Could not delete message: " + ex);
+    }
+
+    if(args == "") {
+      client.logger.warn("Cannot run Command: announce (empty string)");
+      return;
+    }
+    if(args.indexOf())
+    if(urlregex.test(args) || args.indexOf('./src/') == 0) {
+      const img = new Attachment(text);
+      message.channel.send(img);
+      return;
     }
     message.channel.send(text);
-  }
-}
+};
 
 exports.conf = {
 	enabled: true,
